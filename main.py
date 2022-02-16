94b9089ce5cc4377bad04aea943c318842db972f
Ethan <81245565+ethan4748@users.noreply.github.com>
2022-02-16 08:54:34 -0500

    haha eric

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from random import randint
+from random import choice, randint
 import requests
 fout = open ('log.txt', 'w')
 chara = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 link = []
 for i in range(16):
-    x = chara[randint(0, 35)]
+    x = random.choice(chara)
     if (chara.index(x) < 25):
         if (randint(0, 1) == 1):
             link.append(str(x).capitalize())
@@ -21,3 +21,5 @@ if (int(api.status_code) == 429):
     fout.write("Too many requests! https://discord.gift/{code}\n")
 else:
     fout.write(f'https://discord.gift/{code} is invalid!\n')
+    
+fout.close()
